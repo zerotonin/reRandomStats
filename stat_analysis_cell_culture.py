9933329f43a750ce13137078de82e65b4df733b1
Bart Geurten <bart.geurten@otago.ac.nz>
2023-04-06 11:18:17 +1200

    rerun script to make sure things are still correct

--- stat_analysis_cell_culture.py
+++ stat_analysis_cell_culture.py
@@ -26,6 +26,8 @@ mutant zebrafish. All p-values are corrected with the Benjamini Hochberg FDR det
 # file_path contains a list of file paths to csv files containing the original data
 file_path = ['./Data/cell_culture_data.csv']
 
+# data combinations to be tested
+combinations = [('wt_c1', 'mut_c1'), ('wt_c2', 'mut_c2'), ('wt_c3','mut_c3'), ('wt_c4','mut_c4')] 
 
 # Iterate over the file paths
 for i in range(len(file_path)):
@@ -37,7 +39,7 @@ for i in range(len(file_path)):
     # Read the data from the current file and convert it to a long table format
     id_list,data=dio.wide_table_csv_to_long_table(file)
     # Create a multiGroupTest object and set the data, group and test parameters
-    mgt = multiGroupTest.multiGroupTest(data,id_list,'Fisher:exact',0)
+    mgt = multiGroupTest.multiGroupTest(data,id_list,'Fisher:exact','set',combination_set=combinations)
     # Run the main method of the multiGroupTest object
     result_df = mgt.main()
     # Save the result to a csv file
